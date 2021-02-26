e13fa80a1531af7939298e0abac35ce15258f1d2
Santiago Forero <ds.foreroa@uniandes.edu.co>
2021-02-25 19:39:49 -0500

    add/match card and home layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,100 @@
-import React from 'react'
-import NavBar from '../app/components/navbar'
+import React from "react";
+import NavBar from "../app/components/navbar";
 import "../firebase/client";
-import styles from "../app/styles/index.module.css"
-import MatchCard from '../app/components/match_card';
-
+import styles from "../app/styles/index.module.css";
+import MatchCard from "../app/components/match_card";
 
 const Home = () => {
-
-    return (
-        <React.Fragment>
-            <NavBar/>
-            <main className={styles.main}>
-            <h2 className={styles.title}>¡Partidos de Hoy!</h2>
-                <MatchCard hour="7:00 PM"/>
-            </main>
-        </React.Fragment>
-    )
-}
-export default Home
+  return (
+    <React.Fragment>
+      <NavBar />
+      <main className={styles.main}>
+        <h2 className={styles.title}>¡Partidos de Hoy!</h2>
+        <section className={styles.cards_container}>
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={true}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={false}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={false}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={true}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={false}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={true}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={false}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={false}
+          />
+          <MatchCard
+            hour="7:00 PM"
+            competition="Champions League"
+            homeTeamName="FC Barcelona"
+            homeTeamImageUrl="/images/barcelona.png"
+            awayTeamImageUrl="/images/real-madrid.png"
+            awayTeamName="Real Madrid"
+            like={true}
+          />
+        </section>
+      </main>
+    </React.Fragment>
+  );
+};
+export default Home;
