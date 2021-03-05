6e28c8822ebab404f03d309f4bdf7c3a0f738a3b
Pablo Verduzco <pabloverduzcos@gmail.com>
2021-03-04 19:32:42 -0600

    feature/add search bar at navbar component

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -4,29 +4,46 @@ import styles from "./navbar.module.css";
 import Image from "next/image";
 import Button from "../button";
 import Link from "next/link";
-import firebase,{signOut} from "@fire-client";
+import firebase, { signOut } from "@fire-client";
+import SearchBar from "@components/search_bar";
+
+import useDimensions from "../../hooks/useDimensions";
 
 const NavBar = () => {
-
   const [fireUser, setFireUser] = useState<firebase.User>(null);
   firebase.auth().onAuthStateChanged((user) => setFireUser(user));
   const router = useRouter();
 
+  const { width } = useDimensions();
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
+        <a>
+          <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
+        </a>
       </Link>
-      {router.pathname != "/login" &&
-      router.pathname != "/signup" &&
-      !fireUser ? (
-        <ul className={styles.list}>
-          <Button onClick={() => router.push("/login")} secondary={true}>
-            Login
-          </Button>
-          <Button onClick={() => router.push("/signup")}>SignUp</Button>
-        </ul>
-      ) : (fireUser !=undefined)? welcome(fireUser):null }
+      {!!(width >= 768) ? (
+        router.pathname != "/login" &&
+        router.pathname != "/signup" &&
+        !fireUser ? (
+          <SearchBar />
+        ) : null
+      ) : null}
+      {!!(width >= 768) ? (
+        router.pathname != "/login" &&
+        router.pathname != "/signup" &&
+        !fireUser ? (
+          <ul className={styles.list}>
+            <Button onClick={() => router.push("/login")} secondary={true}>
+              Login
+            </Button>
+            <Button onClick={() => router.push("/signup")}>SignUp</Button>
+          </ul>
+        ) : fireUser != undefined ? (
+          welcome(fireUser)
+        ) : null
+      ) : null}
     </nav>
   );
 };
@@ -36,14 +53,14 @@ const welcome = (fireUser: firebase.User) => {
     <ul className={styles.list}>
       <h3 className={styles.name}>{"Bienvenido " + fireUser?.displayName}</h3>
       <Image
-        onClick={()=>signOut()}
+        onClick={() => signOut()}
         src="/images/icons/logout.png"
         alt="Logout"
         width={24}
         height={24}
       />
     </ul>
-  )
-}
+  );
+};
 
 export default NavBar;
