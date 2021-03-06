a6f0d364e1e61d2edc7e141f3630ecc8416a65a9
Pablo Verduzco <pabloverduzcos@gmail.com>
2021-03-05 23:31:45 -0600

    feature/Add global context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Provider } from "../app/context/state";
 import "./../app/styles/globlal.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
