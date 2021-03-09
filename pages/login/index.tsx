0424941e72cc022368aff6896199bb6447c14187
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-03-09 17:37:21 -0500

    add/footer component at Favorite, Profile pages

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,47 +6,45 @@ import LoginLayout from "@components/login_layout";
 import { loginWithEmail } from "@fire-client";
 import { useRouter } from "next/router";
 
-
-
 const Login = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const onClickLogin = async() => {
-    await loginWithEmail(email,password)
-    router.push("/")
-  }
+	const onClickLogin = async () => {
+		await loginWithEmail(email, password);
+		router.push("/");
+	};
 
-  return (
-    <LoginLayout>
-      <h2 className={style.title}>Ingresa a Luxxon</h2>
-      <form className={style.credentials_form}>
-        <input
-          className={style.input}
-          placeholder="Correo electrónico"
-          onChange={(event => setEmail(event.target.value))}
-          type="email"
-        />
-        <input
-          className={style.input}
-          placeholder="Contraseña"
-          onChange={(event => setPassword(event.target.value))}
-          type="password"
-        />
-      </form>
-      <Button onClick={onClickLogin} buttonClass="mt-3 ml-0" >
-        Ingresar
-      </Button>
-      <p>
-        ¿No tienes cuenta?{'  '}
-        <Link href="/signup">
-          <strong>Registrate</strong>
-        </Link>
-      </p>
-    </LoginLayout>
-  );
+	return (
+		<LoginLayout>
+			<h2 className={style.title}>Ingresa a Luxxon</h2>
+			<form className={style.credentials_form}>
+				<input
+					className={style.input}
+					placeholder="Correo electrónico"
+					onChange={(event) => setEmail(event.target.value)}
+					type="email"
+				/>
+				<input
+					className={style.input}
+					placeholder="Contraseña"
+					onChange={(event) => setPassword(event.target.value)}
+					type="password"
+				/>
+			</form>
+			<Button onClick={onClickLogin} buttonClass="mt-3 ml-0">
+				Ingresar
+			</Button>
+			<p>
+				¿No tienes cuenta?{"  "}
+				<Link href="/signup">
+					<strong>Registrate</strong>
+				</Link>
+			</p>
+		</LoginLayout>
+	);
 };
 
 export default Login;
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,55 +1,56 @@
 import NavBar from "@components/navbar";
+import Footer from "@components/footer";
 import Image from "next/image";
 import React, { useState } from "react";
 import styles from "./profile.module.css";
 import firebase from "@fire-client";
 
 const Profile = () => {
-  const [fireUser, setFireUser] = useState<firebase.User>(
-    firebase.auth().currentUser
-  );
+	const [fireUser, setFireUser] = useState<firebase.User>(
+		firebase.auth().currentUser
+	);
 
-  firebase.auth().onAuthStateChanged((user) => setFireUser(user));
-  const [name, setName] = useState(
-    fireUser ? fireUser.displayName : "Bienvenido"
-  );
+	firebase.auth().onAuthStateChanged((user) => setFireUser(user));
+	const [name, setName] = useState(
+		fireUser ? fireUser.displayName : "Bienvenido"
+	);
 
-  const onChangeName = (val: React.ChangeEvent<HTMLInputElement>) => {
-    setName(val.target.value);
-    firebase.auth().currentUser.updateProfile({
-      displayName: val.target.value,
-    });
-  };
+	const onChangeName = (val: React.ChangeEvent<HTMLInputElement>) => {
+		setName(val.target.value);
+		firebase.auth().currentUser.updateProfile({
+			displayName: val.target.value,
+		});
+	};
 
-  
-  return (
-    <React.Fragment>
-      <NavBar />
-      <div className={styles.container}>
-        <div>
-          <h2 className="title text-center">¡Estás en tu perfil!</h2>
-          <Image
-            height="300px"
-            width="300px"
-            alt="profile pic"
-            src="/images/logo.png"
-            className="rounded"
-            layout="responsive"
-          ></Image>
-          <input
-            type="edit"
-            className={styles.input}
-            value={name}
-            onChange={(val) => onChangeName(val)}
-          />
-          <p className={styles.info}>
-            <strong className={styles.info_highlight}>Email: </strong>{" "}
-            {fireUser ? fireUser.email : ""}
-          </p>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<NavBar />
+			<div className={styles.container}>
+				<div>
+					<h2 className="title text-center">¡Estás en tu perfil!</h2>
+					<Image
+						height="300px"
+						width="300px"
+						alt="profile pic"
+						src="/images/logo.png"
+						className="rounded"
+						layout="responsive"
+					></Image>
+					<input
+						type="edit"
+						className={styles.input}
+						value={name}
+						onChange={(val) => onChangeName(val)}
+					/>
+					<p className={styles.info}>
+						<strong className={styles.info_highlight}>Email: </strong>{" "}
+						{fireUser ? fireUser.email : ""}
+					</p>
+				</div>
+			</div>
+			<Footer />
+		</React.Fragment>
+	);
 };
 
 export default Profile;
