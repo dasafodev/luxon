a199031e1d08be6217e02af0da82212bcce1dc1f
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-03-11 16:33:03 -0500

    update/

--- app/components/match_details_layout/index.tsx
+++ app/components/match_details_layout/index.tsx
@@ -6,12 +6,13 @@ const MatchDetails = () => {
     <div>
       <h1 className={styles.title}>Información del partido</h1>
       <div className={styles.container}>
-        <div className={styles.leftTeam}>
+        <div className={styles.containerTeam}>
           <h2 className={styles.nameTeam}>FC Barcelona </h2>
-          <button className={styles.conference} type='button'>
+          <img src='../../../public/images/barcelona.png' alt='Barcelona`s logo' height={140} width={140} />
+          <button className={styles.buttonVideoCall} type='button'>
             <img
               src='../../../public/images/icons/videoConference.png'
-              alt='imagen para conocer hinchas de tú equipo de todo el mundo'
+              alt='Video conference with fans around the world'
               width={50}
               height={50}
             />
@@ -25,7 +26,22 @@ const MatchDetails = () => {
           <p className='bold'>7.00pm</p>
           <p className={styles.competition}>ChampionsLeague</p>
         </div>
-        <div className={styles.rightTeam}>right team</div>
+        <div className={styles.containerTeam}>
+          <h2 className={styles.nameTeam}>Real Madrid </h2>
+          <img src='../../../public/images/real-madrid.png' alt='RealMadrid`s logo' />
+          <button className={styles.buttonVideoCall} type='button'>
+            <img
+              src='../../../public/images/icons/videoConference.png'
+              alt='Video conference with fans around the world'
+              width={50}
+              height={50}
+            />
+          </button>
+          <div className={styles.containerPlayers}>
+            <p className={styles.playerName}>Player name</p>
+            <p className={styles.playerPosition}>Player position</p>
+          </div>
+        </div>
       </div>
     </div>
   );
