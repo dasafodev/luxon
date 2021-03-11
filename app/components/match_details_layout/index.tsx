2925b541cf2f9bff168c6665d89f8cab98f5c051
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-03-11 14:02:33 -0500

    update/advancing

--- app/components/match_details_layout/index.tsx
+++ app/components/match_details_layout/index.tsx
@@ -6,7 +6,17 @@ const MatchDetails = () => {
     <>
       <h1 className={styles.title}>Información del partido</h1>
       <div className={styles.container}>
-        <div className={styles.leftTeam}>left team</div>
+        <div className={styles.leftTeam}>
+          <h2>FC Barcelona </h2>
+          <button type='button'>
+            <img
+              src='../../../public/images/icons/videoConference.png'
+              alt='imagen para conocer hinchas de tú equipo de todo el mundo'
+              width={50}
+              height={50}
+            />
+          </button>
+        </div>
         <div className={styles.time}>
           <p className='bold'>7.00pm</p>
           <p className={styles.competition}>ChampionsLeague</p>
