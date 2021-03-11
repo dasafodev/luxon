b0928aa7a3408e4b1889324f757f37d430e78b9a
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-03-11 15:36:47 -0500

    fix/some typos

--- app/components/match_details_layout/index.tsx
+++ app/components/match_details_layout/index.tsx
@@ -3,12 +3,12 @@ import styles from './match_details_layout.module.css';
 
 const MatchDetails = () => {
   return (
-    <>
+    <div>
       <h1 className={styles.title}>Información del partido</h1>
       <div className={styles.container}>
         <div className={styles.leftTeam}>
-          <h2>FC Barcelona </h2>
-          <button type='button'>
+          <h2 className={styles.nameTeam}>FC Barcelona </h2>
+          <button className={styles.conference} type='button'>
             <img
               src='../../../public/images/icons/videoConference.png'
               alt='imagen para conocer hinchas de tú equipo de todo el mundo'
@@ -16,6 +16,10 @@ const MatchDetails = () => {
               height={50}
             />
           </button>
+          <div className={styles.containerPlayers}>
+            <p className={styles.playerName}>Player name</p>
+            <p className={styles.playerPosition}>Player position</p>
+          </div>
         </div>
         <div className={styles.time}>
           <p className='bold'>7.00pm</p>
@@ -23,7 +27,7 @@ const MatchDetails = () => {
         </div>
         <div className={styles.rightTeam}>right team</div>
       </div>
-    </>
+    </div>
   );
 };
 
