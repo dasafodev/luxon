523dd83311ac0a7ea87bc5fe110045e7d275d034
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-03-11 09:48:12 -0500

    update/feature/matchDetailsLayout

---
+++ app/components/match_details_layout/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './match_details_layout.module';
+
+const MatchDetails = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Información del partido</h1>
+      <div className={styles.container}>
+        <div className={styles.leftTeam}>left team</div>
+        <div className={styles.time}>
+          <p className='bold'>7.00pm</p>
+          <p className={styles.competition}>ChampionsLeague</p>
+        </div>
+        <div className={styles.rightTeam}>right team</div>
+      </div>
+    </>
+  );
+};
+
+export default MatchDetails;
