423f50ae9a21053e856f0539cc963c0d1cd3d614
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-03-11 21:36:28 -0500

    update/404 page -> done

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,12 @@
+import styles from '../app/styles/404.module.css';
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        404 - Opps! la página que buscas esta fuera de lugar! <br />
+        <a href='/'>Continua agendando tus partidos preferidos </a>
+      </h1>
+    </div>
+  );
 }
