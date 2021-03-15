daa0daf06d9f39689a681523fea4953e8f964177
Santiago Forero <ds.foreroa@uniandes.edu.co>
2021-03-15 01:52:47 -0500

    jitsi working

--- pages/live/[id].tsx
+++ pages/live/[id].tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '@components/navbar';
 import Footer from '@components/footer';
 import '../../firebase/client';
 import styles from './live.module.css';
 
 import useDimensions from '../../app/hooks/useDimensions';
+import { Jutsu } from 'react-jutsu';
 
 const Live = (): JSX.Element => {
+  const [room, setRoom] = useState('');
+  const [name, setName] = useState('');
+  const [call, setCall] = useState(false);
+  const [password, setPassword] = useState('');
   const { width } = useDimensions();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (room && name) setCall(true);
+  };
 
   return (
     <React.Fragment>
@@ -17,14 +27,52 @@ const Live = (): JSX.Element => {
         <section className={styles.content}>
           {width >= 768 ? (
             <div className={styles.stream}>
-              <iframe
+              {call ? (
+                <Jutsu
+                  containerStyles={{ width: '500px', height: '800px' }}
+                  roomName={room}
+                  displayName={name}
+                  password={password}
+                  onMeetingEnd={() => console.warn('Meeting has ended')}
+                  loadingComponent={<p>loading ...</p>}
+                  errorComponent={<p>Oops, something went wrong</p>}
+                />
+              ) : (
+                <form>
+                  <input
+                    id='room'
+                    type='text'
+                    placeholder='Room'
+                    value={room}
+                    onChange={(e) => setRoom(e.target.value)}
+                  />
+                  <input
+                    id='name'
+                    type='text'
+                    placeholder='Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <input
+                    id='password'
+                    type='text'
+                    placeholder='Password (optional)'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button onClick={handleClick} type='submit'>
+                    Start / Join
+                  </button>
+                </form>
+              )}
+              {/* <iframe
                 width='560'
                 height='315'
                 src='https://www.youtube.com/embed/Y-NeVAOkMlI'
                 frameBorder='0'
                 allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                 allowFullScreen
-              ></iframe>
+              ></iframe> */}
             </div>
           ) : null}
           <div className={styles.chat}>
