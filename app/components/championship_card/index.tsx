c8a9745e4926911a3f636ab56c78ddedd67929fc
Maorguez <avila.mauro@gmail.com>
2021-03-18 15:02:58 -0500

    update/styles components scroll_slider

--- app/components/championship_card/index.tsx
+++ app/components/championship_card/index.tsx
@@ -4,11 +4,13 @@ import styles from './championship_card.module.css';
 
 const Championship = ({ emblem, champions }) => {
   return (
-    <section className={styles.container}>
-      <div className={styles.card}>
-        <Image src={emblem} alt='Championship' width={180} height={180} />
-        <p>{champions}</p>
+    <section>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <Image src={emblem} alt='Championship' width={180} height={180} className={styles.image} />
+        </div>
       </div>
+      <p className={styles.title}>{champions}</p>
     </section>
   );
 };
