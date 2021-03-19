140f9a356d30e45b2e4afda084b27e7084d3a1a0
Pablo Verduzco <pabloverduzcos@gmail.com>
2021-03-18 21:13:18 -0600

    Add/Connection to Firestore to show recent published messages

--- pages/live/[id].tsx
+++ pages/live/[id].tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '@components/navbar';
 import Footer from '@components/footer';
-import { currentUser } from '@fire-client';
+import firebase, { currentUser } from '@fire-client';
 import 'firebase/auth';
 
 import styles from './live.module.css';
@@ -11,10 +11,17 @@ import { Jutsu } from 'react-jutsu';
 import { useRouter } from 'next/router';
 
 const Live = (): JSX.Element => {
+  const db = firebase.firestore();
+
   const router = useRouter();
+
   const [room, setRoom] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+
+  const [messages, setMessages] = useState([]);
+  const [currentMessage, setCurrentMessage] = useState('');
+
   const { width } = useDimensions();
 
   useEffect(() => {
@@ -22,6 +29,50 @@ const Live = (): JSX.Element => {
     setRoom(router.query.id as string);
     setPassword(router.query.id as string);
   }, []);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('lives')
+      .doc(`${router.query.id}`)
+      .collection('messages')
+      .orderBy('createdAt', 'desc')
+      .limit(1)
+      .onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMessages((prevState) => {
+          if (prevState.some((doc) => doc.id === data[0]?.id)) {
+            return prevState;
+          }
+          return [...prevState, ...data];
+        });
+      });
+
+    return unsubscribe;
+  }, []);
+
+  const handleChange = (event) => {
+    setCurrentMessage(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    const trimmedMessage = currentMessage.trim();
+    if (trimmedMessage) {
+      db.collection('lives')
+        .doc(router.query.id as string)
+        .collection('messages')
+        .add({
+          text: trimmedMessage,
+          author: currentUser()?.displayName,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+      setCurrentMessage('');
+    }
+  };
 
   return (
     <React.Fragment>
@@ -43,12 +94,25 @@ const Live = (): JSX.Element => {
             </div>
           ) : null}
           <div className={styles.chat}>
-            <div className={styles.chat_conversation}></div>
+            <div className={styles.chat_conversation}>
+              <ul className={styles.chat_list}>
+                {messages.map((message) => (
+                  <li key={message.id}>
+                    <p className={styles.chat_message}>
+                      <span className={styles.chat_author}>{message.author}: </span>
+                      {message.text}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
             <div className={styles.chat_box}>
-              <textarea />
-              <button type='button' className={styles.chat_button}>
-                Send
-              </button>
+              <form onSubmit={handleOnSubmit}>
+                <textarea placeholder='Escribe tu mensaje...' value={currentMessage} onChange={handleChange} />
+                <button type='submit' className={styles.chat_button}>
+                  Send
+                </button>
+              </form>
             </div>
           </div>
         </section>
