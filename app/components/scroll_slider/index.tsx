a55511d145d36d715e3aa1f24feaa6b81013506e
Santiago Forero <ds.foreroa@uniandes.edu.co>
2021-03-25 02:15:49 -0500

    flag

--- app/components/scroll_slider/index.tsx
+++ app/components/scroll_slider/index.tsx
@@ -1,40 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { useAppActions } from 'app/context/state';
+import React from 'react';
 import styles from './scroll_slider.module.css';
 import Championship from '../championship_card';
 import Shields from '@components/shields';
 
-const ScrollSlider = () => {
-  const [idCode, setIdCode] = useState('2021');
-  const { setGames, setPositions, setScorers } = useAppActions();
-
-  const handleChampions = (code) => {
-    setIdCode(code);
+export const getServerSideProps = async () => {
+  return {
+    props: {},
   };
+};
 
-  const fetchData = async () => {
-    const response = await fetch(`/api/competitions/${idCode}/matches`);
-    const data = await response.json();
-    setGames(data.matches);
+const ScrollSlider = ({ handleClick }) => {
+  // const [idCode, setIdCode] = useState('2021');
+  // const { setGames, setPositions, setScorers } = useAppActions();
 
-    const res = await fetch(`/api/competitions/${idCode}/standings/`);
-    const positionsTable = await res.json();
-    setPositions(positionsTable.standings[0].table);
+  // const handleChampions = (code) => {
+  //   setIdCode(code);
+  // };
 
-    const resScorers = await fetch(`/api/competitions/${idCode}/scorers/`);
-    const goalScorer = await resScorers.json();
-    setScorers(goalScorer.scorers);
-  };
+  // const fetchData = async () => {
+  //   const response = await fetch(`/api/competitions/${idCode}/matches`);
+  //   const data = await response.json();
+  //   setGames(data.matches);
 
-  useEffect(() => {
-    fetchData().then();
-  }, [idCode]);
+  //   const res = await fetch(`/api/competitions/${idCode}/standings/`);
+  //   const positionsTable = await res.json();
+  //   setPositions(positionsTable.standings[0].table);
+
+  //   const resScorers = await fetch(`/api/competitions/${idCode}/scorers/`);
+  //   const goalScorer = await resScorers.json();
+  //   setScorers(goalScorer.scorers);
+  // };
+
+  // useEffect(() => {
+  //   fetchData().then();
+  // }, [idCode]);
 
   return (
     <section className={styles.container}>
       <div className={styles.scroll}>
         {Shields.map((shield) => (
-          <div key={shield.id} onClick={() => handleChampions(shield.code)}>
+          <div key={shield.id} onClick={() => handleClick(shield.code)}>
             <Championship emblem={shield.image} champions={shield.title} />
           </div>
         ))}
--- pages/competition/index.tsx
+++ pages/competition/index.tsx
@@ -3,15 +3,20 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import firebase from '@fire-client';
 import '../../firebase/client';
-import { useAppContext } from 'app/context/state';
+// import { useAppContext } from 'app/context/state';
 import NavBar from '@components/navbar';
 import styles from '../../app/styles/index.module.css';
 import stylesCompetition from './competition.module.css';
 import GameCard from '@components/game_card';
 import ScrollSlider from '@components/scroll_slider';
+import Shields from '@components/shields';
 
 const Competition = () => {
-  const { games, positions } = useAppContext();
+  const [data, setData] = useState([]);
+  // const [idCode, setIdCode] = useState('2021');
+  // const [matches, setMatches] = useState([]);
+  // const [positions, setPositions] = useState([]);
+  // const { games, positions } = useAppContext();
   const router = useRouter();
   const [, setFireUser] = useState<firebase.User>(firebase.auth().currentUser);
   const cardMatch = [];
@@ -24,18 +29,50 @@ const Competition = () => {
         router.replace('/login');
       }
     });
+    fetchData();
 
     return () => {
       unlisten();
     };
   }, []);
 
+  const fetchData = async () => {
+    for (let index = 0; index < Shields.length; index++) {
+      const idCode = Shields[index].id;
+      const tempData = {};
+      // Shields.forEach((shield) => {
+      const response = await fetch(`/api/competitions/${idCode}/matches`);
+      const dataResp = await response.json();
+      tempData['matches'] = dataResp.matches;
+      // setMatches(data.matches);
+
+      const res = await fetch(`/api/competitions/${idCode}/standings/`);
+      const positionsTable = await res.json();
+      tempData['positions'] = positionsTable.standings[0].table;
+      // setPositions(positionsTable.standings[0].table);
+
+      const resScorers = await fetch(`/api/competitions/${idCode}/scorers/`);
+      const goalScorer = await resScorers.json();
+      // setScorers(goalScorer.scorers);
+      tempData['scorers'] = goalScorer.scorers;
+      tempData['id'] = idCode;
+      setData([...data, tempData]);
+    }
+    console.warn(data);
+  };
+
+  const handleClick = async (newIdCode) => {
+    // setIdCode(newIdCode);
+    // await fetchData(newIdCode);
+    console.warn(newIdCode);
+  };
+
   return (
     <React.Fragment>
       <NavBar />
       <section className={styles.main}>
         <div className={stylesCompetition.scroll}>
-          <ScrollSlider />
+          <ScrollSlider handleClick={handleClick} />
         </div>
         <nav className={stylesCompetition.navCompetition}>
           <ul className={stylesCompetition.list}>
@@ -52,17 +89,19 @@ const Competition = () => {
           </ul>
         </nav>
         <section className={stylesCompetition.cards_container}>
-          {games.map((game) => {
-            const emblemHome = positions.filter((position) => position.team.id === game.homeTeam.id);
-            const emblemAway = positions.filter((item) => item.team.id === game.awayTeam.id);
+          {data[0].matches.map((game) => {
+            const emblemHome = data[0].positions.filter((position) => position.team.id === game.homeTeam.id);
+            const emblemAway = data[0].positions.filter((item) => item.team.id === game.awayTeam.id);
             if (emblemHome.length === 1) {
               cardMatch.push(
                 <GameCard
                   key={game.id}
                   crestUrlHome={emblemHome[0].team.crestUrl}
+                  // crestUrlHome='https://crests.football-data.org/65.svg'
                   homeTeamName={game.homeTeam.name}
                   awayTeamName={game.awayTeam.name}
                   crestUrlAway={emblemAway[0].team.crestUrl}
+                  // crestUrlAway='https://crests.football-data.org/65.svg'
                   scoreHomeTeam={game.score.fullTime.homeTeam}
                   scoreAwayTeam={game.score.fullTime.awayTeam}
                   status={game.status}
