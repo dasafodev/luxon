3974c508526a36038befcedcad8178c74d06b2a9
Pablo Verduzco <pabloverduzcos@gmail.com>
2021-03-25 18:23:58 -0600

    update/remove favorites button if no matches are saved

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -174,7 +174,10 @@ const NavBar = ({ onChange = null }) => {
             LoggedOptions()
           ) : null
         ) : (
-          <ul className={styles.bottom_navbar}>
+          <ul
+            className={styles.bottom_navbar}
+            style={{ gridTemplateColumns: `repeat(${favorites.length > 0 ? '4' : '3'}, 1fr)` }}
+          >
             <li className={styles.bottom_navbar_icons}>
               <Link href='/'>
                 <a role='link'>
@@ -255,44 +258,46 @@ const NavBar = ({ onChange = null }) => {
                 </a>
               </Link>
             </li>
-            <li className={styles.bottom_navbar_icons}>
-              <Link href='/favorites'>
-                <a role='link'>
-                  {router.pathname === '/favorites' ? (
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='32'
-                      height='32'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        fillRule='evenodd'
-                        d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='32'
-                      height='32'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      stroke='currentColor'
-                    >
-                      <path
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
-                      />
-                    </svg>
-                  )}
-                </a>
-              </Link>
-            </li>
+            {favorites.length > 0 && (
+              <li className={styles.bottom_navbar_icons}>
+                <Link href='/favorites'>
+                  <a role='link'>
+                    {router.pathname === '/favorites' ? (
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='32'
+                        height='32'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                      >
+                        <path
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          fillRule='evenodd'
+                          d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='32'
+                        height='32'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
+                        />
+                      </svg>
+                    )}
+                  </a>
+                </Link>
+              </li>
+            )}
             <li className={styles.bottom_navbar_icons}>
               <Link href='/profile'>
                 <a role='link'>
